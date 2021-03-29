3a03fb284c9e97e7930d6907f77bca687d586cdc
iaprofessional <75013595+iaprofessional@users.noreply.github.com>
2021-03-29 12:44:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function login() {
-    user_name=document.getElementById("user_name").value;
+	user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name",user_name);
-    window.location="kwitternextgen_room.html";
-}
+    window.location.replace("https://iaprofessional.github.io/kwitter/kwitternextgen_room.html");
+    }      
+    setTimeout("login()", 20000);
